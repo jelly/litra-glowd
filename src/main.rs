519b835772e50063dfc1c3b6c50af91109b4a211
Jelle van der Waa <jelle@vdwaa.nl>
2022-06-10 13:43:41 +0200

    Initial dbus interface
    Implement a simple dbus interface for controlling the litra glow light.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+use std::sync::{Arc, Mutex};
 use hidapi;
+use zbus::blocking::ConnectionBuilder;
+use zbus::Result;
+use zbus_macros::dbus_interface;
+use event_listener::Event;
 
 const VENDOR_ID: u16 = 0x046d;
 const PRODUCT_ID: u16 = 0xc900;
@@ -15,43 +20,63 @@ fn write(device: &hidapi::HidDevice, mut cmd: Vec<u8>) {
     println!("Wrote: {:?} byte(s)", res);
 }
 
-fn on(device: &hidapi::HidDevice) {
-    write(&device, vec![0x1c, 0x01]);
+struct LitraGlowd {
+    done: Event,
+    device: Arc<Mutex<hidapi::HidDevice>>,
 }
 
-fn off(device: &hidapi::HidDevice) {
-    write(&device, vec![0x1c, 0x00]);
-}
-
-fn brightness(device: &hidapi::HidDevice, brightness: u8) {
-    if brightness > 100 && brightness < 1 {
-        return;
-    }
-    let f_brightness: f32 = brightness.into();
-    let f_level = MIN_BRIGHTNESS + ((f_brightness/100.0) * (MAX_BRIGHTNESS - MIN_BRIGHTNESS)).floor();
-    println!("brightness {}, level {}", brightness, f_level);
-    let level = f_level as u8;
-    write(&device, vec![0x4c, 0x00, level]);
-}
-
-fn temperature(device: &hidapi::HidDevice, temp: u16) {
-    if temp < 2700 && temp < 6500 {
-        return;
+#[dbus_interface(name = "org.jelle.LitraGlowd1")]
+impl LitraGlowd {
+    fn on(&self) {
+        let device = self.device.lock().unwrap();
+        write(&device, vec![0x1c, 0x01]);
     }
 
-    let mut cmd = vec![0x9c];
-    cmd.extend(temp.to_be_bytes().iter());
-    println!("temp {:?}", cmd);
-    write(&device, cmd);
+    fn off(&self) {
+        let device = self.device.lock().unwrap();
+        write(&device, vec![0x1c, 0x00]);
+    }
+
+    fn brightness(&self, brightness: u8) {
+        let device = self.device.lock().unwrap();
+        if brightness > 100 && brightness < 1 {
+            return;
+        }
+        let f_brightness: f32 = brightness.into();
+        let f_level = MIN_BRIGHTNESS + ((f_brightness/100.0) * (MAX_BRIGHTNESS - MIN_BRIGHTNESS)).floor();
+        println!("brightness {}, level {}", brightness, f_level);
+        let level = f_level as u8;
+        write(&device, vec![0x4c, 0x00, level]);
+    }
+
+    fn temperature(&self, temp: u16) {
+        let device = self.device.lock().unwrap();
+        if temp < 2700 && temp < 6500 {
+            return;
+        }
+
+        let mut cmd = vec![0x9c];
+        cmd.extend(temp.to_be_bytes().iter());
+        println!("temp {:?}", cmd);
+        write(&device, cmd);
+    }
 }
 
-fn main() {
-
+fn main() -> Result<()> {
     let api = hidapi::HidApi::new().unwrap();
     let (vid, pid) = (VENDOR_ID, PRODUCT_ID);
-    let device = api.open(vid, pid).unwrap();
-    // on(&device);
-    // brightness(&device, 20);
-    // temperature(&device, 4000);
-    // off(&device);
+    let device = Arc::new(Mutex::new(api.open(vid, pid).unwrap()));
+    let litraglowd = LitraGlowd {
+        done: event_listener::Event::new(),
+        device,
+    };
+    let done_listener = litraglowd.done.listen();
+    let _ = ConnectionBuilder::session()?
+        .name("org.jelle.LitraGlowd")?
+        .serve_at("/org/jelle/LitraGlowd", litraglowd)?
+        .build()?;
+
+    done_listener.wait();
+
+    Ok(())
 }
